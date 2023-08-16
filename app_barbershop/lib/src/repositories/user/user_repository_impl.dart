ac8fb96c00e9069e54f8cb3c9b64013d8716845d
Emerson Melo Martins <emerson25xd@gmail.com>
2023-08-15 21:52:36 -0300

    providers, exceptions, dio rest client, user repository

---
+++ app_barbershop/lib/src/repositories/user/user_repository_impl.dart
@@ -0,0 +1,37 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:app_barbershop/src/core/exceptions/auth_exception.dart';
+import 'package:app_barbershop/src/core/fp/either.dart';
+import 'package:app_barbershop/src/core/restClient/rest_client.dart';
+import 'package:app_barbershop/src/repositories/user/user_repository.dart';
+import 'package:dio/dio.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final RestClient restClient;
+  UserRepositoryImpl({required this.restClient});
+
+  @override
+  Future<Either<AuthException, String>> login(
+      String email, String password) async {
+    try {
+      final Response(:data) = await restClient.unauth.post("/auth", data: {
+        "email": email,
+        "password": password,
+      });
+
+      return Success(data['access_token']);
+    } on DioException catch (e, s) {
+      if (e.response != null) {
+        final Response(:statusCode) = e.response!;
+        if (statusCode == HttpStatus.forbidden) {
+          log("Login ou senha inválidos", error: e, stackTrace: s);
+          return Failure(AuthUnauthorizedError());
+        }
+      }
+
+      log("Erro ao realizar login", error: e, stackTrace: s);
+      return Failure(AuthError(message: "Erro ao realizar login"));
+    }
+  }
+}
